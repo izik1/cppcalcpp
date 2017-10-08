9957ba81b572ab34769e32e5e5945d083a9b6d9f
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-10-07 17:59:38 -0700

    Implement recent

--- cppcalcpp/evaluator.cpp
+++ cppcalcpp/evaluator.cpp
@@ -10,12 +10,16 @@ inline int64_t ipow(int64_t num, int64_t pow) {
 
 int64_t evaluator::evaluateOp(const std::shared_ptr<exprtree> lhs, int64_t rhs, type op) {
     switch(op) {
-    case type::plus: return evaluate(lhs) + rhs;
-    case type::minus: return evaluate(lhs) - rhs;
-    case type::astrisk: return evaluate(lhs) * rhs;
-    case type::slash: return evaluate(lhs) / rhs;
-    case type::carrot: return ipow(evaluate(lhs), rhs);
+    case type::plus: return evaluateInternal(lhs) + rhs;
+    case type::minus: return evaluateInternal(lhs) - rhs;
+    case type::astrisk: return evaluateInternal(lhs) * rhs;
+    case type::slash: return evaluateInternal(lhs) / rhs;
+    case type::carrot: return ipow(evaluateInternal(lhs), rhs);
     case type::equals:
+        if(lhs.get()->m_strval[0] == '_') {
+            throw std::logic_error("identifiers starting with underscores are reserved, "
+                "and cannot be used on the left hand side of an assignment.");
+        }
         ids.insert_or_assign(lhs->m_strval, rhs);
         return ids[lhs->m_strval];
     default: throw std::logic_error("unexpected op");
@@ -23,6 +27,20 @@ int64_t evaluator::evaluateOp(const std::shared_ptr<exprtree> lhs, int64_t rhs, 
 }
 
 int64_t evaluator::evaluate(const std::shared_ptr<exprtree> tree) {
+    auto val = evaluateInternal(tree);
+    for(int i = 10; i > 1; i--) {
+        if(ids.find("_" + std::to_string(i - 1)) != ids.end()) {
+            ids.insert_or_assign("_" + std::to_string(i), ids["_" + std::to_string(i - 1)]);
+        }
+    }
+
+    ids.insert_or_assign("_", val);
+    ids.insert_or_assign("_1", val);
+
+    return val;
+}
+
+int64_t evaluator::evaluateInternal(const std::shared_ptr<exprtree> tree) {
     if(isOp(tree->m_type)) {
         assert(tree->subtrees.size() == 2);
         return evaluateOp(tree->subtrees[0], evaluate(tree->subtrees[1]), tree->m_type);
--- cppcalcpp/evaluator.h
+++ cppcalcpp/evaluator.h
@@ -3,8 +3,10 @@
 #include "exprtree.h"
 class evaluator {
 public:
-    std::unordered_map<std::string, int64_t> ids;
     evaluator();
     int64_t evaluate(const std::shared_ptr<exprtree> tree);
+private:
+    int64_t evaluateInternal(const std::shared_ptr<exprtree> tree);
+    std::unordered_map<std::string, int64_t> ids;
     int64_t evaluateOp(const std::shared_ptr<exprtree> lhs, int64_t rhs, type op);
 };
